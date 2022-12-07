ed0e39df21ee4bdd00da6a0c78462a72b2658b23
Andrés Páez --casa-- <andresgam31@gmail.com>
2022-12-07 12:21:29 -0500

    cap 81 finalizado

---
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
@@ -0,0 +1,10 @@
+import hamburguerMenu from "./menu_hamburgesas.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburguerMenu(".panel-btn",".panel",".menu a");
+  })
+
+  // Boton de hamburguesa ↑↑↑
+  
