164ed087571fb31469d3b287112ce4f1323c4f5b
Andrés Páez --casa-- <andresgam31@gmail.com>
2022-12-08 15:38:19 -0500

    cap 82 finish

--- java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
@@ -1,10 +1,9 @@
 import hamburguerMenu from "./menu_hamburgesas.js";
+import { digitalClock } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-  hamburguerMenu(".panel-btn",".panel",".menu a");
+  hamburguerMenu(".panel-btn", ".panel", ".menu a");//Del documento hamburguesa del primer ejercicio.
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj"); //Para iniciar el reloj // cap del reloj 82
   })
-
-  // Boton de hamburguesa ↑↑↑
-  
