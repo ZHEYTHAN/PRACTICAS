60c26e05d618ff34aaa934bff30db4a6081d7dce
Andrés Páez --casa-- <andresgam31@gmail.com>
2022-12-14 12:03:41 -0500

    fin cap 89

--- java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
@@ -14,26 +14,25 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarma("/java/js/soluciónes de ejercicios de jon mircha/assets/sonidos/niebla.mp3", "#activar-alarma", "#desactivar-alarma") //Cap 83
   countdown("countdown", "jun 04, 2023 18:00:00", "Feliz cumpleaños Andrés"); //Como solo se va a trabajar con un id y como se está invocado directo, no necesita el simbolo # --- ya que en la otra hoja de js se estará usando el              .getElementById
   scrollTopButton(".scroll-top-btn"); // 87
-  darktheme(".dark-theme-btn", "dark-mode")
 });
 
-  //Capitulo 84 -- aprendizaje de los shortCuts ↓↓↓↓
-  /*
-  Hay tres eventos para el teclado.
+//Capitulo 84 -- aprendizaje de los shortCuts ↓↓↓↓
+/*
+Hay tres eventos para el teclado.
 
-  * keydown: Es cuando se oprime cualquier tecla.
-  * keyup: Es cuando se deja de oprimir cualquier tecla.
-  * keypress: Es cuando se mantiene oprimido cualquier tecla.
- 
-  */
+* keydown: Es cuando se oprime cualquier tecla.
+* keyup: Es cuando se deja de oprimir cualquier tecla.
+* keypress: Es cuando se mantiene oprimido cualquier tecla.
 
-  //Para verlo se ejecuta la function shortcuts(e) // esta función es de la hora teclado.js (importado de ahí).
+*/
+
+//Para verlo se ejecuta la function shortcuts(e) // esta función es de la hora teclado.js (importado de ahí).
 // * keydown: Es cuando se oprime cualquier tecla.↓↓
 
 /* 
 
 });
- */
+*/
 
 
 /*
@@ -57,3 +56,5 @@ d.addEventListener("keydown", e => {
   shortcuts(e);
   moveBall(e, ".ball", ".stage"); //Al ser clases, es importanten ponerles el punto al inicio, sino me va a generar un error.
 })
+
+darktheme(".dark-theme-btn", "dark-mode"); //Se saco del DOMContentLoaded ya que en su js se va a usar en la funcion otro DOMContentLoaded y es para que no genere conflicto.
--- java/js/soluciónes de ejercicios de jon mircha/js-jon/tema_oscuro.js
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/tema_oscuro.js
@@ -1,6 +1,7 @@
 //Capitulo 88
 
-const d = document;
+const d = document,
+  ls = localStorage;
 
 export default function darktheme(btn, classDark){
   const $themeBtn = d.querySelector(btn),
@@ -10,21 +11,53 @@ export default function darktheme(btn, classDark){
   
   let moon = "🌛",
     sun = "🌞";
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Del cap 89 ↓↓
+  const lightMode = () => {
+            $selectors.forEach(el => el.classList.remove(classDark));
+    $themeBtn.textContent = moon;
+    ls.setItem("theme", "light");
+  };
+  const darkMode = () => {$selectors.forEach(el => el.classList.add(classDark));
+    $themeBtn.textContent = sun;
+    ls.setItem("theme", "dark");
+  };
+
+   //Del cap 89 ↑↑
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
   d.addEventListener("click", e => {
     if (e.target.matches(btn)) {
       console.log($themeBtn.textContent);
       if ($themeBtn.textContent === moon) {
-        $selectors.forEach(el => el.classList.add(classDark));
-        $themeBtn.textContent = sun;
+        darkMode();
       } else {
-        $selectors.forEach(el => el.classList.remove(classDark));
-        $themeBtn.textContent = moon;
+        lightMode();
       }
     }
   });
   
-}
-
 //Falta el consumo d Api para el local storage para que se conserve el theme que se deje seleccionado.
 //Sigue cap 89
+//Consumo de APi localStorage.
+
+  d.addEventListener("DOMContentLoaded", e => {
+    console.log(ls.getItem("theme"));
+    if (ls.getItem("theme") === null) {
+      ls.setItem("theme", "light");
+    }
+      
+    if (ls.getItem("theme") === "light") {
+      lightMode();
+    }
+
+    if (ls.getItem("theme") === "dark") {
+      darkMode();
+    }
+
+  });
+  
+}
+
+//Sigue el cap 90.
