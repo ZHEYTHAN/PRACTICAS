57a02ecf72196071e941a6462186b64373ac58bc
Andrés Páez --casa-- <andresgam31@gmail.com>
2022-12-26 15:24:43 -0500

    fin de cap 100

--- java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/index_dom.js
@@ -14,6 +14,7 @@ import scrollSpy from "./scroll_espia.js";
 import draw from "./sorteo.js";
 import { shortcuts, moveBall } from "./teclado.js"; //Importa el cap 84 de aprendizaje de los shortCuts
 import darktheme from "./tema_oscuro.js";
+import smartVideo from "./video_inteligente.js";
 
 const d = document;
 
@@ -60,6 +61,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
   slider(); //CAPITULO 98
 
   scrollSpy(); //CAPITULO 99 --- SCROLL ESPIA.
+
+  smartVideo(); //CAPITULO 100 -- VIDEO INTELIGENTE
 });
 
 //Capitulo 84 -- aprendizaje de los shortCuts ↓↓↓↓
