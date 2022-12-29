82f2edae9b3fac09bbdc40f7b48f17f50b1ca454
Andrés Páez --casa-- <andresgam31@gmail.com>
2022-12-29 11:49:23 -0500

    FIN DE CAP 102

--- java/js/soluciónes de ejercicios de jon mircha/js-jon/validaciones_formulario.js
+++ java/js/soluciónes de ejercicios de jon mircha/js-jon/validaciones_formulario.js
@@ -7,7 +7,37 @@ const d = document,
 export default function contactFormValidations() {
   const $form = d.querySelector(".contact-form"),
     $inputs = d.querySelectorAll(".contact-form [required]");
-  console.log($inputs);
-}
 
-// MINUTO 5
+  // console.log($inputs);
+
+  $inputs.forEach((input) => {
+    const $span = d.createElement("span");
+    $span.id = input.name;
+    $span.textContent = input.title;
+    $span.classList.add("contact-form-error", "none");
+    input.insertAdjacentElement("afterend", $span);
+  });
+
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(".contact-form [required]")) {
+      let $input = e.target,
+        // el interruptor de corto circuito || es para que o valide el pattern ("Que va a generar error ya que en el textArea no aplica un pattern"), O ue tome el data-attribute (que si es valido accediendo de esa forma.)
+        pattern = $input.pattern || $input.dataset.pattern; //El dataset es para poder acceder a los data attribute de los html
+
+      // console.log($input, pattern);
+
+      if (pattern && $input.value !== "") {
+        let regex = new RegExp(pattern);
+        return !regex.exec($input.value)
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+
+      if (!pattern) {
+        return $input.value === ""
+          ? d.getElementById($input.name).classList.add("is-active")
+          : d.getElementById($input.name).classList.remove("is-active");
+      }
+    }
+  });
+}
