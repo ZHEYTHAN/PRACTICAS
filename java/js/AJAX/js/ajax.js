1356acdcc2f4334c3c571d5f30938e26d5e1587a
Andrés Páez <andresgam31@gmail.com>
2023-01-02 17:59:59 -0500

    cap 106 incompleto

---
+++ java/js/AJAX/js/ajax.js
@@ -0,0 +1,29 @@
+//CAPITULO 106
+//Para que AJAX funcione, si o si debe estar en servidor (Aplica y funciona el servidor local de live server) porque si no, no va a funcionar.
+//Se va a generar consumo de una API externa.
+//DESCARGAR JSON FORMATTER PARA EXTENSIÓN DE CHROME EN EQUIPO DE CASA. ---- 
+
+d = document;
+
+(()=>{
+const xhr =new XMLHttpRequest(),
+$xhr = d.getElementById("xhr"),
+$fragment = d.createDocumentFragment();
+//
+//
+//
+console.log(xhr);
+//
+//
+//
+//Esto requiere de tres parámetros, el primero esta declarado en la variable de xhr (XMLHttpRequest()), sel segundo es el evento "readystatechange" y el tercero es una callback que para este caso será el evento  e=>{}
+xhr.addEventListener("readystatechange", e =>{})
+
+//Luego el evento OPEN en la variable.
+
+xhr.open()
+
+
+})();
+
+//min 18
