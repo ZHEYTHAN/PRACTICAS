0714be852e8736cb09d387488cc4208a42f8a780
ANDRES PAEZ <andresgam31@gmail.com>
2023-01-05 17:48:40 -0500

    fin de capitulo 108

--- java/js/AJAX/js/fetch_async_await.js
+++ java/js/AJAX/js/fetch_async_await.js
@@ -2,37 +2,44 @@
 //El mismo fetch pero se dara uso de Async y Await.
 
 (() => {
-  const d = document;
-
-  const $fetchAsync = d.getElementById("fetch-async"),
-    $fragment = d.createDocumentFragment();
+  const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
 
   //
   //
 
   async function getData() {
     try {
-      let respuesta = await fetch("https://jsonplaceholder.typicode.com/users"),
-        json = await respuesta.json();
+      let respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+      let json = await respuesta.json();
+      //
+      // console.log(respuesta, json);
+      //
+      //Para generar el error se va a hacer una sola condicionar aquí, ya que throw redirecciona el mensaje al catch.
+      if (!respuesta.ok)
+        throw { status: respuesta.status, statusText: respuesta.statusText };
       //
       //
       //
 
-
-      json.forEach((el)=>{
-        const $li = d.createElement("li");
-        $li.innerHTML = `${el.name} --- ${el-email} --- ${el.phone} `;
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} --- ${el.email} --- ${el.phone} `;
         $fragment.appendChild($li);
-      })
+      });
 
       $fetchAsync.appendChild($fragment);
-      
+      //
+      //
+      //
     } catch (elError) {
+      //
+      //
+      let message = elError.statusText || "HA OCURRIDO UN ERROR";
+      $fetchAsync.innerHTML = `Error ${elError.status}: ${message}`;
     } finally {
     }
   }
 
   getData();
 })();
-
-//MIN 7
