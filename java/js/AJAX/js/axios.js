404fad30e0d335c3fee88b1e4e3bc05ccdec7b77
ANDRES PAEZ <andresgam31@gmail.com>
2023-01-06 12:24:32 -0500

    FIN DE CAP 109

---
+++ java/js/AJAX/js/axios.js
@@ -0,0 +1,57 @@
+//CAPITULO 109
+
+//AXIOS:
+
+(() => {
+  const d = document;
+
+  const $axios = d.getElementById("axios"),
+    $fragment = d.createDocumentFragment();
+  //
+  //
+  //.then() se usaría para trabajar la respuesta de la peticion .get que se está haciendo.
+  //.catch() para manejar el error.
+
+  axios
+    .get(`https://jsonplaceholder.typicode.com/users`)
+    //
+    //
+    .then((respuesta) => {
+      console.log(respuesta);
+      //
+      //
+      //
+      let json = respuesta.data;
+      //
+      //
+      json.forEach((el) => {
+        const $li = d.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      //
+      //
+      //
+      $axios.appendChild($fragment);
+    })
+    //
+    //
+    .catch((error) => {
+      // .response es de la librería de axios --- ver manejo de errores (handling errors) para ver eso.
+      // console.log(error.response);
+      //
+      //
+      //
+      let message = error.response.statusText || "Ocurrio un error";
+      $axios.innerHTML = `Error ${error.response.status}: ${message}`;
+    })
+    //
+    //
+    .finally(() => {
+      // console.log(
+      //   "Aquí se ejecuta el código que se requiera independientemente de la respuesta del then o del catch"
+      // );
+    });
+  //
+  //
+})();
--- java/js/AJAX/js/fetch.js
+++ java/js/AJAX/js/fetch.js
@@ -15,34 +15,33 @@
 
     //.json()  en fetch reemplazaría lo que en XMLHttpRequest seria el .parse() para hacer que el/los documentos .json sean leíbles en formato js
     //Es decir que se vuelvan un arreglo [].
-    .then((respuesta)=> respuesta.ok ? respuesta.json(): Promise.reject(respuesta))
+    .then((respuesta) =>
+      respuesta.ok ? respuesta.json() : Promise.reject(respuesta)
+    )
     .then((json) => {
-      console.log(json);
+      // console.log(json);
       // $fetch.innerHTML = json;
       //
       //
-      json.forEach((el)=>{
+      json.forEach((el) => {
         const $li = d.createElement("li");
         $li.innerHTML = `${el.name} *** ${el.email} *** ${el.phone}`;
         $fragment.appendChild($li);
-      })
+      });
       //
       //
 
       $fetch.appendChild($fragment);
-
-      
     })
     .catch((error) => {
-      // console.log(error); 
+      // console.log(error);
 
-    //
-    //
-    let message = error.statusText || "Ocurrio un error";
-    $fetch.innerHTML = `Error   **** ${error.status}: ${message}`
+      //
+      //
+      let message = error.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error   **** ${error.status}: ${message}`;
     })
-    .finally(() =>
-      console.log("Esto se ejecutará independientemente de la promesa de fetch")
-    );
+    .finally(() => {
+      // console.log("Esto se ejecutará independientemente de la promesa de fetch")
+    });
 })();
-
